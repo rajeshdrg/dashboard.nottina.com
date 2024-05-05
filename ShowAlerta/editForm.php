6db12ddfac4077ccf54f6cee831d642c6ab0330f
Rajesh-Rampersad <danigarcia.negocio@gmail.com>
2024-05-05 18:55:11 -0300

    debug

--- ShowAlerta/editForm.php
+++ ShowAlerta/editForm.php
@@ -7,13 +7,13 @@
     <title>Formulario de Edición</title>
 
     <!-- Bootstrap CSS -->
-    <!-- <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 
-    jQuery -->
-    <!-- <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script> -->
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
 
     <!-- Bootstrap JS -->
-    <!-- <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 
     <!-- Estilos personalizados para este formulario -->
 </head>
