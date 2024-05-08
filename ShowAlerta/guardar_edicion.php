be2cf076b39195fe707de9ba3866a7098720a5fd
rajesh <rajesh@hugtak.com>
2024-05-08 16:51:04 -0300

    debug

--- ShowAlerta/guardar_edicion.php
+++ ShowAlerta/guardar_edicion.php
@@ -1,34 +1,37 @@
 <?php
 
-if($_SERVER['DOCUMENT_ROOT']==null)
+if ($_SERVER['DOCUMENT_ROOT'] == null)
     $_SERVER['DOCUMENT_ROOT'] = "..";
 
 require_once $_SERVER['DOCUMENT_ROOT'] . "/erpme/banco/sqldatareader.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/erpme/banco/sqlcommand.php";
 
-class GuardarEdicion {
-    
+class GuardarEdicion
+{
+
     public $conexao;
 
-    function __construct() {
-        require $_SERVER['DOCUMENT_ROOT'].'/erpme/banco/conecta.php';
+    function __construct()
+    {
+        require $_SERVER['DOCUMENT_ROOT'] . '/erpme/banco/conecta.php';
         $this->conexao = $conexao;
     }
-    
-    function guardarEdicion($codAlerta, $prioridade, $quando, $modulo, $item, $valor, $descricao, $analista) {
+
+    function guardarEdicion($codAlerta, $quando,  $analista)
+    {
         // Actualizar la entrada correspondiente en la base de datos de alertas
-        
+
         $Sql = new SqlCommand("Sql");
         $Sql->connection = $this->conexao;
-        
+
         // Aquí debes escribir las consultas SQL necesarias para actualizar los campos de la alerta
         // Por ejemplo:
-        $Sql->query = "UPDATE alerta SET prioridade = $1, quando = $2, modulo = $3, item = $4, valor = $5, descricao = $6, analista = $7 WHERE cod_alerta = $8";
-        $Sql->params = array($prioridade, $quando, $modulo, $item, $valor, $descricao, $analista, $codAlerta);
+        $Sql->query = "UPDATE alerta SET analista = $1, quando = $2 WHERE cod_alerta = $3";
+        $Sql->params = array($$analista, $quando, $codAlerta);
         $Sql->Execute();
-        
+
         // Puedes agregar comprobaciones adicionales, manejo de errores, etc., según sea necesario
-        
+
         // Redireccionar a la página de visualización de alertas
         header("Location: /ShowAlerta.php");
         exit();
@@ -37,15 +40,11 @@ class GuardarEdicion {
 
 // Instanciar la clase y llamar a la función guardarEdicion con los datos del formulario
 $guardarEdicion = new GuardarEdicion();
-if(isset($_POST['cod_alerta'])) {
+if (isset($_POST['cod_alerta'])) {
     $guardarEdicion->guardarEdicion(
         $_POST['cod_alerta'],
-        $_POST['prioridade'],
-        $_POST['quando'],
-        $_POST['modulo'],
-        $_POST['item'],
-        $_POST['valor'],
-        $_POST['descricao'],
-        $_POST['analista']
+        $_POST['analista'],
+        $_POST['quando']
+
     );
 }
