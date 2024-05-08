087b29174253a604e32c4e63c531a684da8365ca
rajesh <rajesh@hugtak.com>
2024-05-08 14:36:06 -0300

    debug

--- ShowAlerta/editForm.php
+++ ShowAlerta/editForm.php
@@ -5,15 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/ShowAlerta/editForm.css">
-    <link href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
     <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
-
-    <!-- Optional theme -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
-
-    <!-- Latest compiled and minified JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 
     <title>EditAlerta</title>
 
@@ -61,6 +54,9 @@
         </div>
 
     </section> -->
+    <script src="../js/jquery-1.10.2.js"></script>
+    <script src="../js/jquery-3.3.1.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
 </body>
 
 </html>
