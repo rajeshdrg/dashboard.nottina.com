0e12909eb9fff9ad1df110e4933380e616082983
rajesh <rajesh@hugtak.com>
2024-05-08 15:58:05 -0300

    debug

--- ShowAlerta/editForm.php
+++ ShowAlerta/editForm.php
@@ -4,16 +4,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- <link rel="stylesheet" href="/ShowAlerta/editForm.css"> -->
+    <link rel="stylesheet" href="/ShowAlerta/editForm.css">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-    <!-- jQuery (necesario para Bootstrap JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-
-    <!-- Bootstrap JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
 
     <title>EditAlerta</title>
 
