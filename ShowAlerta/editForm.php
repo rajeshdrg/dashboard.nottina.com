9fd7d9e9f570e117e99cd1dd7b5a9ca3b830d8d7
rajesh <rajesh@hugtak.com>
2024-05-10 09:11:17 -0300

    debug

--- ShowAlerta/editForm.php
+++ ShowAlerta/editForm.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alerta</title>
-    <link rel="stylesheet" href="/ShowAlerta/editForm.css">
-    <link rel="stylesheet" href="/ShowAlerta/editForm.js">
+    <link rel="stylesheet" href="../ShowAlerta/editForm.css">
+    <link rel="stylesheet" href="../ShowAlerta/editForm.js">
 </head>
 
 <body>
