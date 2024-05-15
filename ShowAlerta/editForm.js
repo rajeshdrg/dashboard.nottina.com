5043600e04f47799d62029ceb0c0f7b78ae99f7b
rajesh <rajesh@hugtak.com>
2024-05-15 09:08:15 -0300

    fin

--- ShowAlerta/editForm.js
+++ ShowAlerta/editForm.js
@@ -1,32 +1,78 @@
 
-// Obtiene la ventana modal y el botón de abrir
-var modal = document.getElementById("myModal");
-var openModalBtn = document.getElementById("openModalBtn");
-var closeBtn = document.getElementsByClassName("close")[0];
+        document.addEventListener("DOMContentLoaded", function() {
+            var modal = document.getElementById("myModal");
+            modal.style.display = "block"; //Mostrar o modal ao carregar a página
 
-// Abre la ventana modal cuando se hace clic en el botón
-openModalBtn.onclick = function () {
-    modal.style.display = "block";
-}
-
-// Cierra la ventana modal cuando se hace clic en el botón de cerrar
-closeBtn.onclick = function () {
-    modal.style.display = "none";
-}
-
-// Cierra la ventana modal cuando se hace clic fuera de ella
-window.onclick = function (event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
-// Envía el formulario al servidor
-var form = document.getElementById("editForm");
-form.addEventListener("submit", function (event) {
-    event.preventDefault(); // Evita que se recargue la página al enviar el formulario
-    // Aquí puedes agregar el código para enviar los datos del formulario al servidor utilizando AJAX
-    // Por ejemplo, puedes usar fetch() o XMLHttpRequest
-});
+            var closeButton = document.getElementsByClassName("close")[0];
+            closeButton.addEventListener("click", function() {
+                modal.style.display = "none"; // Ocultar o modal ao clicar no botão de fechamento
+            });
+        });
 
 
+        var form = document.getElementById("editForm");
+
+        form.addEventListener("submit", function(event) {
+            event.preventDefault(); // Impedir que a página seja recarregada ao enviar o formulário
+
+            var formData = new FormData(form);
+            console.log("Datos del formulario:", {
+                cod_alerta: formData.get('cod_alerta'),
+                analista: formData.get('analista'),
+                fechamento: formData.get('fechamento')
+            });
+
+
+            Swal.fire({
+                title: 'Tem certeza?',
+                text: "Deseja atualizar os dados do alerta?",
+                icon: 'warning',
+                showCancelButton: true,
+                confirmButtonText: 'Sim, atualizar',
+                cancelButtonText: 'Não, cancelar'
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    fetch('/ShowAlerta/guardar_edicion.php', {
+                        method: 'POST',
+                        body: formData
+                    })
+                    .then(response => response.json())
+                    .then(data => {
+                        if (data.success) {
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Os campos foram alterados corretamente',
+                                showConfirmButton: true,
+                                confirmButtonText: 'Fechar'
+                            }).then(() => {
+                                window.location.href = '../index.php';
+                            });
+
+
+                        } else {
+                            Swal.fire({
+                                icon: 'error',
+                                title: 'Erro',
+                                text: data.message
+                            }).then(() => {
+                                window.location.href = '../index.php';
+                            });
+                        }
+                    })
+                    .catch(error => {
+                        console.error('Erro ao enviar solicitação:', error);
+                        Swal.fire({
+                            icon: 'error',
+                            title: 'Erro ao enviar solicitação',
+                            text: 'Ocorreu um erro ao enviar a solicitação. Por favor, tente novamente mais tarde.'
+                        }).then(() => {
+                            window.location.href = '../index.php';
+                        });
+                    });
+
+                    
+                } else if (result.dismiss === Swal.DismissReason.cancel) {
+                    window.location.href = '../index.php';
+                }
+            });
+        });
--- ShowAlerta/editForm.php
+++ ShowAlerta/editForm.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Alerta</title>
     <link rel="stylesheet" href="/ShowAlerta/editForm.css">
+    <link rel="stylesheet" href="/ShowAlerta/editForm.js">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="/js/sweetalert2.all.js"></script>
 </head>
@@ -31,78 +32,6 @@
         </div>
     </div>
 
-    <script>
-        document.addEventListener("DOMContentLoaded", function() {
-            var modal = document.getElementById("myModal");
-            modal.style.display = "block"; //Mostrar o modal ao carregar a página
-
-            var closeButton = document.getElementsByClassName("close")[0];
-            closeButton.addEventListener("click", function() {
-                modal.style.display = "none"; // Ocultar o modal ao clicar no botão de fechamento
-            });
-        });
-
-        var form = document.getElementById("editForm");
-
-        form.addEventListener("submit", function(event) {
-            event.preventDefault(); // Impedir que a página seja recarregada ao enviar o formulário
-
-            var formData = new FormData(form);
-            console.log("Datos del formulario:", {
-                cod_alerta: formData.get('cod_alerta'),
-                analista: formData.get('analista'),
-                fechamento: formData.get('fechamento')
-            });
-
-            Swal.fire({
-                title: '¿Está seguro?',
-                text: "Deseja atualizar os dados do alerta?",
-                icon: 'warning',
-                showCancelButton: true,
-                confirmButtonText: 'Sim, atualizar',
-                cancelButtonText: 'Não, cancelar'
-            }).then((result) => {
-                if (result.isConfirmed) {
-                    fetch('/ShowAlerta/guardar_edicion.php', {
-                        method: 'POST',
-                        body: formData
-                    })
-                    .then(response => response.json())
-                    .then(data => {
-                        if (data.success) {
-                            Swal.fire({
-                                icon: 'success',
-                                title: 'Os campos foram alterados corretamente',
-                                showConfirmButton: true,
-                                confirmButtonText: 'Fechar'
-                            }).then(() => {
-                                window.location.href = '../index.php';
-                            });
-                        } else {
-                            Swal.fire({
-                                icon: 'error',
-                                title: 'Erro',
-                                text: data.message
-                            }).then(() => {
-                                window.location.href = '../index.php';
-                            });
-                        }
-                    })
-                    .catch(error => {
-                        console.error('Erro ao enviar solicitação:', error);
-                        Swal.fire({
-                            icon: 'error',
-                            title: 'Erro ao enviar solicitação',
-                            text: 'Ocorreu um erro ao enviar a solicitação. Por favor, tente novamente mais tarde.'
-                        }).then(() => {
-                            window.location.href = '../index.php';
-                        });
-                    });
-                } else if (result.dismiss === Swal.DismissReason.cancel) {
-                    window.location.href = '../index.php';
-                }
-            });
-        });
-    </script>
+    
 </body>
 </html>
