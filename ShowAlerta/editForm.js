ee40817610bd4f86e4f22f20803cda3479c31c23
rajesh <rajesh@hugtak.com>
2024-05-20 14:21:49 -0300

    debug

--- ShowAlerta/editForm.js
+++ ShowAlerta/editForm.js
@@ -1,15 +1,215 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     // Mostrar o modal no carregamento da página
+
+//     var modal = document.getElementById("myModal");
+
+//     modal.style.display = "block";
+
+//     // Carregue os analistas do backend e preencha o select 
+//     fetch('/ShowAlerta/selectAn.php?action=obter_analistas')
+
+//         .then(response => response.json())
+
+//         .then(data => {
+//             console.log(data);
+//             if (data.success) {
+//                 const select = document.getElementById('analista');
+//                 data.analistas.forEach(analista => {
+//                     const option = document.createElement('option');
+//                     option.value = analista.cod_usuario;
+//                     option.textContent = analista.nome;
+//                     select.appendChild(option);
+//                     document.addEventListener("DOMContentLoaded", function () {
+//                         // Mostrar o modal no carregamento da página
+//                         var modal = document.getElementById("myModal");
+//                         modal.style.display = "block";
+
+//                         // Carregue os analistas do backend e preencha o select 
+//                         fetch('/ShowAlerta/selectAn.php?action=obter_analistas')
+//                             .then(response => response.json())
+//                             .then(data => {
+//                                 console.log(data);
+//                                 if (data.success) {
+//                                     const select = document.getElementById('analista');
+//                                     data.analistas.forEach(analista => {
+//                                         const option = document.createElement('option');
+//                                         option.value = analista.cod_usuario;
+//                                         option.textContent = analista.nome;
+//                                         select.appendChild(option);
+//                                     });
+//                                 } else {
+//                                     console.error('Erro ao obter analistas:', data.message);
+//                                 }
+//                             })
+//                             .catch(error => {
+//                                 console.error('Erro ao carregar analistas:', error);
+//                             });
+//                     });
+
+//                     var form = document.getElementById("editForm");
+
+//                     form.addEventListener("submit", function (event) {
+//                         event.preventDefault(); // Impedir que a página seja recarregada ao enviar o formulário
+
+//                         var formData = new FormData(form);
+//                         var formObject = {
+//                             cod_alerta: formData.get('cod_alerta'),
+//                             analista: formData.get('analista'),
+//                             fechamento: formData.get('fechamento')
+//                         };
+
+//                         console.log("Datos del formulario:", formObject);
+
+//                         Swal.fire({
+//                             title: 'Tem certeza?',
+//                             text: "Deseja atualizar os dados do alerta?",
+//                             icon: 'warning',
+//                             showCancelButton: true,
+//                             confirmButtonText: 'Sim, atualizar',
+//                             cancelButtonText: 'Não, cancelar'
+//                         }).then((result) => {
+//                             console.log(result);
+
+//                             if (result.isConfirmed) {
+//                                 fetch('/ShowAlerta/guardar_edicion.php', {
+//                                     method: 'POST',
+//                                     headers: {
+//                                         'Content-Type': 'application/json'
+//                                     },
+//                                     body: JSON.stringify(formObject) // Enviar o objeto convertido a JSON
+//                                 })
+//                                     .then(response => response.json())
+//                                     .then(data => {
+//                                         console.log("Resultado de la solicitud: ", data);
+//                                         if (data.success) {
+//                                             Swal.fire({
+//                                                 icon: 'success',
+//                                                 title: 'Os campos foram alterados corretamente',
+//                                                 showConfirmButton: true,
+//                                                 confirmButtonText: 'Fechar'
+//                                             }).then((result) => {
+//                                                 if (result.isConfirmed) {
+//                                                     window.location.href = '../index.php';
+//                                                 }
+//                                             });
+//                                         } else {
+//                                             Swal.fire({
+//                                                 icon: 'error',
+//                                                 title: 'Erro',
+//                                                 text: data.message
+//                                             }).then(() => {
+//                                                 window.location.href = '../index.php';
+//                                             });
+//                                         }
+//                                     })
+//                                     .catch(error => {
+//                                         console.error('Erro ao enviar solicitação:', error);
+//                                         Swal.fire({
+//                                             icon: 'error',
+//                                             title: 'Erro ao enviar solicitação',
+//                                             text: 'Ocorreu um erro ao enviar a solicitação. Por favor, tente novamente mais tarde.'
+//                                         }).then(() => {
+//                                             window.location.href = '../index.php';
+//                                         });
+//                                     });
+//                             } else {
+//                                 window.location.href = '../index.php';
+//                             }
+//                         });
+//                     });
+
+//                 });
+
+//             } else {
+//                 console.error('Erro ao obter analistas:', data.message);
+//             }
+//         })
+//         .catch(error => {
+//             console.error('Erro ao carregar analistas:', error);
+//         });
+// });
+
+// var form = document.getElementById("editForm");
+
+// form.addEventListener("submit", function (event) {
+//     event.preventDefault(); // Impedir que a página seja recarregada ao enviar o formulário
+
+//     var formData = new FormData(form);
+//     console.log("Datos del formulario:", {
+//         cod_alerta: formData.get('cod_alerta'),
+//         analista: formData.get('analista'),
+//         fechamento: formData.get('fechamento')
+//     });
+
+//     Swal.fire({
+//         title: 'Tem certeza?',
+//         text: "Deseja atualizar os dados do alerta?",
+//         icon: 'warning',
+//         showCancelButton: true,
+//         confirmButtonText: 'Sim, atualizar',
+//         cancelButtonText: 'Não, cancelar'
+//     }).then((result) => {
+//         console.log(result);
+
+//         if (result.isConfirmed) {
+//             fetch('/ShowAlerta/guardar_edicion.php', {
+//                 method: 'POST',
+//                 headers: {
+//                     'Content-Type': 'application/json'
+//                 },
+//                 body: JSON.stringify(formData)
+//             })
+//                 .then(response => response.json())
+//                 .then(data => {
+//                     console.log("Resultado de la solicitud: ", data);
+//                     if (data.success) {
+//                         Swal.fire({
+//                             icon: 'success',
+//                             title: 'Os campos foram alterados corretamente',
+//                             showConfirmButton: true,
+//                             confirmButtonText: 'Fechar'
+//                         }).then((result) => {
+//                             if (result.isConfirmed) {
+//                                 window.location.href = '../index.php';
+//                             }
+//                         });
+//                     } else {
+//                         Swal.fire({
+//                             icon: 'error',
+//                             title: 'Erro',
+//                             text: data.message
+//                         }).then(() => {
+//                             window.location.href = '../index.php';
+//                         });
+//                     }
+//                 })
+//                 .catch(error => {
+//                     console.error('Erro ao enviar solicitação:', error);
+//                     Swal.fire({
+//                         icon: 'error',
+//                         title: 'Erro ao enviar solicitação',
+//                         text: 'Ocorreu um erro ao enviar a solicitação. Por favor, tente novamente mais tarde.'
+//                     }).then(() => {
+//                         window.location.href = '../index.php';
+//                     });
+//                 });
+//         } else {
+//             window.location.href = '../index.php';
+//         }
+//     });
+// });
+
+
+
+
 document.addEventListener("DOMContentLoaded", function () {
     // Mostrar o modal no carregamento da página
-
     var modal = document.getElementById("myModal");
-
     modal.style.display = "block";
 
     // Carregue os analistas do backend e preencha o select 
     fetch('/ShowAlerta/selectAn.php?action=obter_analistas')
-
         .then(response => response.json())
-
         .then(data => {
             console.log(data);
             if (data.success) {
@@ -19,9 +219,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     option.value = analista.cod_usuario;
                     option.textContent = analista.nome;
                     select.appendChild(option);
-
                 });
-
             } else {
                 console.error('Erro ao obter analistas:', data.message);
             }
@@ -37,11 +235,13 @@ form.addEventListener("submit", function (event) {
     event.preventDefault(); // Impedir que a página seja recarregada ao enviar o formulário
 
     var formData = new FormData(form);
-    console.log("Datos del formulario:", {
+    var formObject = {
         cod_alerta: formData.get('cod_alerta'),
         analista: formData.get('analista'),
         fechamento: formData.get('fechamento')
-    });
+    };
+
+    console.log("Datos del formulario:", formObject);
 
     Swal.fire({
         title: 'Tem certeza?',
@@ -52,17 +252,18 @@ form.addEventListener("submit", function (event) {
         cancelButtonText: 'Não, cancelar'
     }).then((result) => {
         console.log(result);
+
         if (result.isConfirmed) {
             fetch('/ShowAlerta/guardar_edicion.php', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(formData)
+                body: JSON.stringify(formObject) // Enviar o objeto convertido a JSON
             })
                 .then(response => response.json())
                 .then(data => {
-                    console.log(data);
+                    console.log("Resultado de la solicitud: ", data);
                     if (data.success) {
                         Swal.fire({
                             icon: 'success',
@@ -99,4 +300,3 @@ form.addEventListener("submit", function (event) {
         }
     });
 });
-
