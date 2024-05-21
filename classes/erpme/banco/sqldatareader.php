f57512f20e71b08d988725ebc9773968fe1c8bc0
rajesh <rajesh@hugtak.com>
2024-05-21 10:31:52 -0300

    debug

--- classes/erpme/banco/sqldatareader.php
+++ classes/erpme/banco/sqldatareader.php
@@ -1,27 +1,33 @@
 <?php
 
-class SqlDataReader {
+class SqlDataReader
+{
 	public $name;
 	public $Result;
 	public $Rows;
 	public $HasRows;
-	
-	function SqlDataReader() {
+
+	function __construct()
+	{
 		$this->HasRows = false;
 	}
-	function SetResult($Result) {
-			if($Result) {
-			if(pg_num_rows($Result)> 0) {
+	function SetResult($Result)
+	{
+		if ($Result) {
+			if (pg_num_rows($Result) > 0) {
 				$this->Rows = pg_num_rows($Result);
 				$this->HasRows = true;
 				$this->Result = $Result;
 			}
 		}
 	}
-	function GetObject() {
-		if($this->HasRows==false) return null;
+	function GetObject()
+	{
+		if ($this->HasRows == false)
+			return null;
 		return pg_fetch_object($this->Result);
-		
+
 	}
-};
+}
+;
 ?>
--- erpme/data/operadora.php
+++ erpme/data/operadora.php
@@ -2,38 +2,41 @@
 require_once $_SERVER["DOCUMENT_ROOT"] . "/erpme/banco/sqlcommand.php";
 require_once $_SERVER["DOCUMENT_ROOT"] . "/erpme/ui/dropdownlist.php";
 
-class operadora {
-	public $name;
-        public $DropDownList;
-	
-	public function operadora($name,$connection) {
-            
-            $sql = new SqlCommand("$name"."_sql");
-            $sql->connection= $connection;
-            $sql->query="select cod_operadora,operadora from operadora order by operadora";
+class operadora
+{
+    public $name;
+    public $DropDownList;
 
-            $this->DropDownList = new DropDownList($name);
-            $this->DropDownList->SqlCommand = $sql;
-            $this->DropDownList->bind();
-            $this->DropDownList->Value = "cod_operadora";
-            $this->DropDownList->Text  = "operadora";
-            $this->DropDownList->size  = 1;
-            $this->DropDownList->itens  = array(0 => "Todas Operadoras");
-	}
-	
-	public function ShowMe()
-        {
-            $this->DropDownList->ShowMe();
-        } 
-    
-    	public function __invoke()
-        {
-            $this->ShowMe();
-        }
-   
-	public function Set($value)
-        {
-            $this->DropDownList->Set($value);
-        }
-};
+    public function __construct($name, $connection)
+    {
+
+        $sql = new SqlCommand("$name" . "_sql");
+        $sql->connection = $connection;
+        $sql->query = "select cod_operadora,operadora from operadora order by operadora";
+
+        $this->DropDownList = new DropDownList($name);
+        $this->DropDownList->SqlCommand = $sql;
+        $this->DropDownList->bind();
+        $this->DropDownList->Value = "cod_operadora";
+        $this->DropDownList->Text = "operadora";
+        $this->DropDownList->size = 1;
+        $this->DropDownList->itens = array(0 => "Todas Operadoras");
+    }
+
+    public function ShowMe()
+    {
+        $this->DropDownList->ShowMe();
+    }
+
+    public function __invoke()
+    {
+        $this->ShowMe();
+    }
+
+    public function Set($value)
+    {
+        $this->DropDownList->Set($value);
+    }
+}
+;
 ?>
