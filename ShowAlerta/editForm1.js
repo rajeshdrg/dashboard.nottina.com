8be7b511006d3ff3c632ae2ac971de06df7fcdc0
rajesh <rajesh@hugtak.com>
2024-05-21 09:14:22 -0300

    debug

--- ShowAlerta/editForm1.js
+++ ShowAlerta/editForm1.js
@@ -1,16 +1,11 @@
-
 document.addEventListener("DOMContentLoaded", function () {
-    var today = new Date().toISOString().split('T')[0];
-    var fechamentoInput = document.getElementById('fechamento');
-    fechamentoInput.setAttribute('min', today);
-    // Set max date if needed, for example 1 year from today
-    var maxDate = new Date();
-    maxDate.setFullYear(maxDate.getFullYear() + 1);
-    fechamentoInput.setAttribute('max', maxDate.toISOString().split('T')[0]);
+    var modal = document.getElementById("myModal");
+    modal.style.display = "block";
 
     fetch('/ShowAlerta/selectAn.php?action=obter_analistas')
         .then(response => response.json())
         .then(data => {
+            console.log(data);
             if (data.success) {
                 const select = document.getElementById('analista');
                 data.analistas.forEach(analista => {
@@ -34,11 +29,9 @@ form.addEventListener("submit", event => {
     event.preventDefault(); // Impedir que a página seja recarregada ao enviar o formulário
 
     var formData = new FormData(form);
-    var formObject = {
-        cod_alerta: formData.get('cod_alerta'),
-        analista: formData.get('analista'),
-        fechamento: formData.get('fechamento')
-    };
+    var formObject = Object.fromEntries(formData.entries());
+
+    console.log("Datos del formulario:", formObject);
 
     Swal.fire({
         title: 'Tem certeza?',
@@ -49,44 +42,71 @@ form.addEventListener("submit", event => {
         cancelButtonText: 'Não, cancelar'
     })
         .then((result) => {
+            console.log(result);
+
             if (result.isConfirmed) {
                 fetch('/ShowAlerta/guardar_edicion.php', {
+
                     method: 'POST',
-                    mode: "cors",
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify(formObject)
+
                 })
+
+
                     .then(response => {
+                        console.log("HTTP response:", response);
+
                         if (!response.ok) {
                             throw new Error('Network response was not ok');
                         }
-                        return response.json();
+                        return response.text(); // Aquí puede estar ocurriendo el error
                     })
-                    .then(data => {
-                        if (data.success) {
-                            Swal.fire({
-                                icon: 'success',
-                                title: 'Os campos foram alterados corretamente',
-                                showConfirmButton: true,
-                                confirmButtonText: 'Fechar'
-                            }).then((result) => {
-                                if (result.isConfirmed) {
+
+                    .then(text =>{
+                        console.log("Raw response text:", text);
+
+                        try {
+                            var  data = JSON.parse(text);
+                            console.log(data);
+
+                            if (data.success) {
+                                Swal.fire({
+                                    icon: 'success',
+                                    title: 'Os campos foram alterados corretamente',
+                                    showConfirmButton: true,
+                                    confirmButtonText: 'Fechar'
+                                }).then((result) => {
+                                    if (result.isConfirmed) {
+                                        window.location.href = '../index.php';
+                                    }
+                                });
+                            } else {
+                                Swal.fire({
+                                    icon: 'error',
+                                    title: 'Erro',
+                                    text: data.message
+                                }).then(() => {
                                     window.location.href = '../index.php';
-                                }
-                            });
-                        } else {
+                                });
+                            }
+                            
+                        } catch (error) {
+                            console.error('Erro ao analisar JSON:', error);
                             Swal.fire({
                                 icon: 'error',
                                 title: 'Erro',
-                                text: data.message
+                                text: 'Resposta do servidor invalido'
                             }).then(() => {
                                 window.location.href = '../index.php';
                             });
+                            
                         }
                     })
                     .catch(error => {
+                        console.error('Erro ao enviar solicitação:', error);
                         Swal.fire({
                             icon: 'error',
                             title: 'Erro ao enviar solicitação',
@@ -100,3 +120,4 @@ form.addEventListener("submit", event => {
             }
         });
 });
+
