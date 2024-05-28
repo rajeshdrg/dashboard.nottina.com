fb42b2ee4f1ab61b55094263606c6d175fa9a0b8
rajesh <rajesh@hugtak.com>
2024-05-28 11:03:53 -0300

    debug

--- Esms/Esms.php
+++ Esms/Esms.php
@@ -1,84 +1,91 @@
 <?php
-require_once $_SERVER["DOCUMENT_ROOT"]."/modulo/modulo.php";
-require_once $_SERVER["DOCUMENT_ROOT"]."/alerta/alerta.php";
-require_once $_SERVER["DOCUMENT_ROOT"]."/GatewaySMS/GatewaySMS.php";
-require_once $_SERVER["DOCUMENT_ROOT"]."/Esms/EsmsAlerta.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/modulo/modulo.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/alerta/alerta.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/GatewaySMS/GatewaySMS.php";
+require_once $_SERVER["DOCUMENT_ROOT"] . "/Esms/EsmsAlerta.php";
 
-class Esms extends modulo {
+class Esms extends modulo
+{
 
 
-	public $server;
-        public $gateway;
-        public $alerta;
- 
-    
-    public function __construct() {
-        
-        
+    public $server;
+    public $gateway;
+    public $alerta;
+
+
+    public function __construct()
+    {
+
+
         parent::__construct();
-        
-        
-        $this->name  = "Esms";
+
+
+        $this->name = "Esms";
         $this->sigla = "E-SMS";
         $this->icone = "fa fa-mobile";
-        
-        $this->gateway[0] = new GatewaySMS('Curitiba',"/dados/cap/status/status_pr.xml");
-        $this->gateway[1] = new GatewaySMS('São Paulo',"/dados/cap/status/status_sp.xml");
-        $this->gateway[2] = new GatewaySMS('Kannel51',"/dados/cap/status/status_kannel51.xml");
-        
+
+        $this->gateway[0] = new GatewaySMS('Curitiba', "/dados/cap/status/status_pr.xml");
+        $this->gateway[1] = new GatewaySMS('São Paulo', "/dados/cap/status/status_sp.xml");
+        $this->gateway[2] = new GatewaySMS('Kannel51', "/dados/cap/status/status_kannel51.xml");
+        $this->gateway[3] = new GatewaySMS('AML RJ 13', "/dados/cap/status/status_kannel51.xml");
+
         $this->alerta[0] = new EsmsAlerta("/dados/cap/status/alerta.xml");
-        
-        
-        
-        
+
+
+
+
     }
-    
-    public function get_data() {
-        
+
+    public function get_data()
+    {
+
         $hora = date('G');
 
-        foreach($this->gateway as $s) {
+        foreach ($this->gateway as $s) {
             $s->get_data();
             $s->emergencia = true;
         }
-        foreach($this->alerta as $sf) {
+        foreach ($this->alerta as $sf) {
             $sf->get_data();
         }
-        
-    }
-    
-    public function front_call() {
-        
-         parent::front_call();
 
     }
-    
-    public function back_call() {
+
+    public function front_call()
+    {
+
+        parent::front_call();
+
+    }
+
+    public function back_call()
+    {
         $this->get_data();
     }
-    
-    public function ShowMe() {
-        date_default_timezone_set ("America/Sao_Paulo");
+
+    public function ShowMe()
+    {
+        date_default_timezone_set("America/Sao_Paulo");
         $hora = date('G');
-        if($hora > 19 || $hora < 6 ) 
+        if ($hora > 19 || $hora < 6)
             $dark = "dark";
-        else 
-        	$dark = "";
-        
-        foreach($this->gateway as $g) {
+        else
+            $dark = "";
+
+        foreach ($this->gateway as $g) {
             $g->ShowMe();
         }
-        foreach($this->alerta as $sf) {
+        foreach ($this->alerta as $sf) {
             $sf->ShowMe();
         }
-        
-    }    
-    
+
+    }
+
 }
 
 $esms = new Esms();
 
-if(isset($_GET['back'])) {
+if (isset($_GET['back'])) {
     $esms->back_call();
     $esms->ShowMe();
 }
