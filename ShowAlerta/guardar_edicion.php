ee60f13ffb667cef0191c96418f9df07622d0c3a
rajesh <rajesh@hugtak.com>
2024-06-13 14:58:57 -0300

    debug

--- ShowAlerta/guardar_edicion.php
+++ ShowAlerta/guardar_edicion.php
@@ -15,7 +15,7 @@ if ($_SERVER['DOCUMENT_ROOT'] == null) {
 require_once $_SERVER['DOCUMENT_ROOT'] . "/erpme/banco/sqldatareader.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/erpme/banco/sqlcommand.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/erpme/ui/dropdownlist.php";
-
+var_dump($_POST);
 class GuardarEdicion
 {
     public $conexao;
