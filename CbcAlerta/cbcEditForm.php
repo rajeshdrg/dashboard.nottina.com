74c59d93947013384a3a56ed55a6d068838ec885
rajesh <rajesh@hugtak.com>
2024-06-27 17:26:54 -0300

    debug

--- CbcAlerta/cbcEditForm.php
+++ CbcAlerta/cbcEditForm.php
@@ -3,7 +3,7 @@ var_dump($_GET);
 // Recuperar el ID de la URL de manera segura
 $id_xml = filter_input(INPUT_GET, 'id', FILTER_SANITIZE_STRING);
 
-if ($id_xml === null || $id_xml === false) {
+if ($id_xml === null) {
     exit("ID da alerta não fornecido ou inválido.");
 }
 
