9df69579b172ae3e5ceb14c1c7a440e17af370b6
rajesh <rajesh@hugtak.com>
2024-07-02 14:28:00 -0300

    debug

--- CbcAlerta/searchCbc.php
+++ CbcAlerta/searchCbc.php
@@ -29,6 +29,10 @@ class Search
         $data_inicio_db = date_format(date_create_from_format('m/d/y', $data_inicio), 'Y-m-d');
         $data_fim_db = date_format(date_create_from_format('m/d/y', $data_fim), 'Y-m-d');
 
+        // Converter datas para o formato apropriado para o banco de dados (yy/mm/dd)
+        $data_inicio_db = date_format(date_create_from_format('m/d/y', $data_inicio), 'Y-m-d');
+        $data_fim_db = date_format(date_create_from_format('m/d/y', $data_fim), 'Y-m-d');
+
         // Crie a consulta SQL dinamicamente com base nos parâmetros fornecidos
         $sql = "SELECT id_xml, estado, operadora, mme_amf, tecnologia, status, teste, roteamento, to_char(created_at, 'YYYY-MM-DD') AS created_at FROM cbc_relatorio WHERE 1=1";
         $params = [];
