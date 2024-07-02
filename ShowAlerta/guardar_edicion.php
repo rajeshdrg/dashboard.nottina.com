4f85695541133aa79ca852e037b47e44de1b4162
rajesh <rajesh@hugtak.com>
2024-07-02 13:19:45 -0300

    debug

--- ShowAlerta/guardar_edicion.php
+++ ShowAlerta/guardar_edicion.php
@@ -1,16 +1,22 @@
 <?php
-
 header('Content-Type: application/json');
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Methods: POST');
+header("Access-Control-Allow-Origin: *");
 
-if ($_SERVER['DOCUMENT_ROOT'] == null)
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+ini_set('error_log', $_SERVER['DOCUMENT_ROOT'] . '/logs/php_errors.log');
+error_reporting(E_ALL);
+
+if ($_SERVER['DOCUMENT_ROOT'] == null) {
     $_SERVER['DOCUMENT_ROOT'] = "..";
+}
 
 require_once $_SERVER['DOCUMENT_ROOT'] . "/erpme/banco/sqldatareader.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/erpme/banco/sqlcommand.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/erpme/ui/dropdownlist.php";
 
-class Search
+class GuardarEdicion
 {
     public $conexao;
 
@@ -20,71 +26,57 @@ class Search
         $this->conexao = $conexao;
     }
 
-    public function search($estado, $operadora, $tecnologia, $data_inicio, $data_fim)
+    function guardarEdicion($codAlerta, $fechamento, $analista, $codUsuario)
     {
+        if (empty($codAlerta) || empty($analista) || empty($fechamento) || empty($codUsuario)) {
+            echo json_encode(['success' => false, 'message' => 'Erro: Todos os campos são obrigatórios']);
+            exit();
+        }
+
+        // Transformar a data de 'fechamento' para incluir a hora atual
+        $dataCompleta = $fechamento . ' ' . date('H:i:s');
+        $dataCompleta = date('Y-m-d H:i:s', strtotime($dataCompleta));
+
+        // Atualizar o registro na tabela alerta
         $sqlCommand = new SqlCommand("Sql");
         $sqlCommand->connection = $this->conexao;
 
-        // Converter datas para o formato apropriado para o banco de dados (yy/mm/dd)
-        $data_inicio_db = date_format(date_create_from_format('m/d/y', $data_inicio), 'Y-m-d');
-        $data_fim_db = date_format(date_create_from_format('m/d/y', $data_fim), 'Y-m-d');
+        $sqlCommand->query = "
+            UPDATE alerta
+            SET fechamento = TO_TIMESTAMP($2, 'YYYY-MM-DD HH24:MI:SS'),  -- Asegúrate que TO_TIMESTAMP funcione según tu motor de base de datos
+                cod_usuario = $1
+            WHERE cod_alerta = $3
+        ";
 
-        // Crie a consulta SQL dinamicamente com base nos parâmetros fornecidos
-        $sql = "SELECT id_xml, estado, operadora, mme_amf, tecnologia, status, teste, roteamento, to_char(created_at, 'YYYY-MM-DD') AS created_at FROM cbc_relatorio WHERE 1=1";
-        $params = [];
-        $paramIndex = 1;
-
-        if (!empty($estado)) {
-            $sql .= " AND UPPER(estado) = $" . $paramIndex++;
-            $params[] = strtoupper($estado);
-        }
-        if (!empty($operadora)) {
-            $sql .= " AND UPPER(operadora) = $" . $paramIndex++;
-            $params[] = strtoupper($operadora);
-        }
-        if (!empty($tecnologia)) {
-            $sql .= " AND UPPER(tecnologia) = $" . $paramIndex++;
-            $params[] = $tecnologia;
-        }
-        if (!empty($data_inicio) && !empty($data_fim)) {
-            $sql .= " AND created_at BETWEEN $" . $paramIndex++ . " AND $" . $paramIndex++;
-            $params[] = $data_inicio_db;
-            $params[] = $data_fim_db;
-        } elseif (!empty($data_inicio)) {
-            $sql .= " AND created_at >= $" . $paramIndex++;
-            $params[] = $data_inicio_db;
-        } elseif (!empty($data_fim)) {
-            $sql .= " AND created_at <= $" . $paramIndex++;
-            $params[] = $data_fim_db;
-        }
-
-        $sqlCommand->query = $sql;
-        $sqlCommand->params = $params;
+        $sqlCommand->params = array($codUsuario, $dataCompleta, $codAlerta);
 
         try {
-            $result = $sqlCommand->Execute();
-            $data = [];
-
-            while ($row = pg_fetch_assoc($result)) {
-                $data[] = $row;
-            }
-
-            echo json_encode(['success' => true, 'results' => $data]);
+            $sqlCommand->Execute();
+            echo json_encode(['success' => true]);
         } catch (Exception $e) {
             echo json_encode(['success' => false, 'message' => 'Erro ao executar consulta: ' . $e->getMessage()]);
+            exit();
         }
     }
 }
 
-
-$estado = isset($_GET['estado']) ? strtoupper($_GET['estado']) : null;
-$operadora = isset($_GET['operadora']) ? strtoupper($_GET['operadora']) : null;
-$tecnologia = isset($_GET['tecnologia']) ? $_GET['tecnologia'] : null;
-$data_inicio = isset($_GET['data_inicio']) ? $_GET['data_inicio'] : null;
-$data_fim = isset($_GET['data_fim']) ? $_GET['data_fim'] : null;
+$rawPostData = file_get_contents("php://input");
+$data = json_decode($rawPostData, true);
 
 
-$search = new Search();
-$search->search($estado, $operadora, $tecnologia, $data_inicio, $data_fim);
 
-?>
+if (!empty($data['cod_alerta']) && !empty($data['analista']) && !empty($data['fechamento']) && !empty($data['cod_usuario'])) {
+    $guardarEdicion = new GuardarEdicion();
+
+    $guardarEdicion->guardarEdicion(
+        $data['cod_alerta'],
+        $data['fechamento'],
+        $data['analista'],
+        $data['cod_usuario']
+    );
+} else {
+    echo json_encode(['success' => false, 'message' => 'Não se receberam dados do formulário']);
+}
+
+
+
