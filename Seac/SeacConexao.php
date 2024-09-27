b852707cf7ce73e2f34d82521e1ac3da37ebe089
rajesh <rajesh@hugtak.com>
2024-09-27 16:22:13 -0300

    "Classe SeacConexao refatorada: adicionada uma instrução continue no método ShowMe para pular a operadora 'sky'."

--- Seac/SeacConexao.php
+++ Seac/SeacConexao.php
@@ -1,71 +1,78 @@
 <?php
 
-class SeacConexao {
-    
+class SeacConexao
+{
+
     public $file;
     public $xml;
     public $file_date;
-    
-    public function __construct($file) {
-        
+
+    public function __construct($file)
+    {
+
         $this->file = $file;
-        
+
     }
-    
-    
-     public function get_data() {
+
+    public function get_data()
+    {
         $len = filesize($this->file);
-        
-        date_default_timezone_set ("America/Sao_Paulo");
-        $this->file_date  =  date ( "d/m/Y H:i:s.", filemtime($this->file));
-	$arq = fopen($this->file,"r");
-	if($arq==null) {
-		print "Erro: arquivo xml não encontrado<br>;";
-		exit(0);
-	}
-	$xmlstr = fread($arq,$len);
-	fclose($arq);
-        
-        $this->xml= new SimpleXMLElement($xmlstr);
-        
 
-         
-     }
-    
-    public function ShowMe() {
-        date_default_timezone_set ("America/Sao_Paulo");
+        date_default_timezone_set("America/Sao_Paulo");
+        $this->file_date = date("d/m/Y H:i:s.", filemtime($this->file));
+        $arq = fopen($this->file, "r");
+        if ($arq == null) {
+            print "Erro: arquivo xml não encontrado<br>;";
+            exit(0);
+        }
+        $xmlstr = fread($arq, $len);
+        fclose($arq);
+
+        $this->xml = new SimpleXMLElement($xmlstr);
+
+    }
+
+    public function ShowMe()
+    {
+        date_default_timezone_set("America/Sao_Paulo");
         $hora = date('G');
-        if($hora > 19 || $hora < 6 ) 
+        if ($hora > 19 || $hora < 6) {
             $dark = "dark";
-        else 
-        	$dark = "";
-        
-        print  "<div class='card $dark'>";
-	print "<header class=r'card-header'>";
-	print "<b>SEaC - Conexão</b><br>";
-        print  "<span> Atualização $this->file_date</span>";
+        } else {
+            $dark = "";
+        }
+
+        print "<div class='card $dark'>";
+        print "<header class=r'card-header'>";
+        print "<b>SEaC - Conexão</b><br>";
+        print "<span> Atualização $this->file_date</span>";
         print "</header>";
-	print "<div class='card-content'>";
+        print "<div class='card-content'>";
+
+        foreach ($this->xml->heartbeat as $hb) {
+          
+            if ($hb->operadora == "sky") {
+                continue; 
+            }
         
-    foreach($this->xml->heartbeat as $hb) {
+            print "<p>";
+        
+            if ($hb->status == "OK") {
+                print "<span style='display:inline-block; width:90px;'><font color=green>";
+            } else {
+                print "<span style='display:inline-block; width:90px;'><font color=red>";
+            }
+        
+            print "<b>" . $hb->operadora . "</b></span>";
+            print "</font>";
+        
+            print "</p>";
+        }
+        
+        print "</div>";
+        
+        print "</div>";
 
-        print "<p>";
-
-        if($hb->status == "OK") 
-            print  "<span style='display:inline-block; width:90px;'><font color=green>";
-	else 
-            print  "<span style='display:inline-block; width:90px;'><font color=red>";
-        print "<b>".$hb->operadora."</b></span>";
-        print "</font>";
-
-        print "</p>";
     }
 
-        print "</div>";
-        print "</div>";
-
-    }
-    
 }
-
-
